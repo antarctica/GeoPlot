6071eb0d81618fa74487ab10fca900f86bfc3a7b
Ulvetanna <jon_smith83@hotmail.co.uk>
2022-10-20 15:45:52 +0100

    Updating for Python 3.7 compliance

--- bas_geoplot/__init__.py
+++ bas_geoplot/__init__.py
@@ -1,5 +1,5 @@
 name = "bas_geoplot"
-__version__ = "0.0.1a"
+__version__ = "0.0.2"
 __description__ = "GeoPlot: BAS AI Lab plotting scripts built on Folium"
 __license__ = "MIT"
 __author__ = "BAS AI Lab" 
