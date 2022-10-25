ee16f90e788b3b10ad87140dd0c8001d42678e56
Jonathan Smith <jon_smith83@hotmail.co.uk>
2022-10-25 09:11:49 +0100

    Updating _init_.py

--- bas_geoplot/__init__.py
+++ bas_geoplot/__init__.py
@@ -1,6 +1,6 @@
 name = "bas_geoplot"
 __version__ = "0.0.8"
-__description__ = "GeoPlot: BAS AI Lab plotting scripts built on Folium"
+__description__ = "GeoPlot: BAS AI Lab plotting scripts built on Folium "
 __license__ = "MIT"
 __author__ = "BAS AI Lab" 
 __email__ = "jonsmi@bas.ac.uk"
