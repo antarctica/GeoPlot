f11b3e24c5b7a3f4a67ada46511e32f136919f21
hjabbot <42661345+hjabbot@users.noreply.github.com>
2024-03-28 11:37:32 +0000

    Incrementing version number

--- bas_geoplot/__init__.py
+++ bas_geoplot/__init__.py
@@ -1,5 +1,5 @@
 name = "bas_geoplot"
-__version__ = "0.0.25"
+__version__ = "0.0.26"
 __description__ = "GeoPlot: BAS AI Lab plotting scripts built on Folium"
 __license__ = "MIT"
 __author__ = "BAS AI Lab" 
