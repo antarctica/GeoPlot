657b1dc002bd4768971d0840627bbcce67c1709b
George <gecoomb@bas.ac.uk>
2024-04-10 13:58:49 +0100

    Typo in __init__.py

--- bas_geoplot/__init__.py
+++ bas_geoplot/__init__.py
@@ -1,7 +1,7 @@
 name = "bas_geoplot"
 __description__ = "GeoPlot: BAS AI Lab plotting scripts built on Folium"
 __version__ = "0.1.0"
-__description__ = "GeoPlot: BAS AI Lab plotting scripts for internal plotting & visulisation"
+__description__ = "GeoPlot: BAS AI Lab plotting scripts for internal plotting & visualisation"
 __license__ = "MIT"
 __author__ = "George Coombs, Jonathan Smith, Samuel Hall, Harrison Abbot - Software Developers\n Jonathan Smith - Technical Lead\n Maria Fox - Project Manager\n George Coombs - Codebase Champion"
 __email__ = "amop@bas.ac.uk"
