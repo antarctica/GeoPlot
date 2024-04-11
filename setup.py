53f385b1bfe3b10caf7c35847df82c1650847d14
George <gecoomb@bas.ac.uk>
2024-04-11 10:37:53 +0100

    Reformat list of classifiers in setup.py

--- setup.py
+++ setup.py
@@ -12,18 +12,7 @@ def get_content(filename):
 
 requirements = get_content("requirements.txt")
 
-setup(
-    name=bas_geoplot.__name__,
-    version=bas_geoplot.__version__,
-    description=bas_geoplot.__description__,
-    long_description=get_content("README.md"),
-    long_description_content_type="text/markdown",
-    author=bas_geoplot.__author__,
-    author_email=bas_geoplot.__email__,
-    maintainer=bas_geoplot.__author__,
-    maintainer_email=bas_geoplot.__email__,
-    url="https://github.com/antarctica/GeoPlot",
-    classifiers=[el.lstrip() for el in """
+print([el.lstrip() for el in """
         Development Status :: 3 - Alpha
         Intended Audience :: Science/Research
         Intended Audience :: System Administrators
@@ -35,7 +24,30 @@ setup(
         Programming Language :: Python :: 3.8
         Programming Language :: Python :: 3.9
         Topic :: Scientific/Engineering
-    """.split('\n')],
+    """.split('\n')])
+
+setup(
+    name=bas_geoplot.__name__,
+    version=bas_geoplot.__version__,
+    description=bas_geoplot.__description__,
+    long_description=get_content("README.md"),
+    long_description_content_type="text/markdown",
+    author=bas_geoplot.__author__,
+    author_email=bas_geoplot.__email__,
+    maintainer=bas_geoplot.__author__,
+    maintainer_email=bas_geoplot.__email__,
+    url="https://github.com/antarctica/GeoPlot",
+    classifiers=[el.lstrip() for el in """Development Status :: 3 - Alpha
+        Intended Audience :: Science/Research
+        Intended Audience :: System Administrators
+        License :: OSI Approved :: MIT License
+        Natural Language :: English
+        Operating System :: OS Independent
+        Programming Language :: Python
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3.8
+        Programming Language :: Python :: 3.9
+        Topic :: Scientific/Engineering""".split('\n')],
     data_files= glob.glob('bas_geoplot/config/**'),
     entry_points={
         'console_scripts': [
